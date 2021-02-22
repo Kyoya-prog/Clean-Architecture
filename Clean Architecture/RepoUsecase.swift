77ed9a0d92c0ef5a08292e98ee8a2b83a17ed515
kyoya <kyoyaprog@gmail.com>
2021-02-22 17:07:57 +0900

    RepoUsecaseOutputProtocolの作成

--- Clean Architecture/RepoUsecase.swift
+++ Clean Architecture/RepoUsecase.swift
@@ -9,7 +9,20 @@ import Foundation
 
 /// リポジトリ検索　Usecase Input
 protocol RepoUsecaseProtocol{
+    /// リポジトリ検索を開始する
+    /// - parameter keyword:検索キーワード
     func startSearch(keyword:String)
+}
+
+/// リポジトリ検索　Usecase Output
+protocol RepoUsecaseOutputProtocol{
+    /// 検索結果を表示する
+    /// - parameter results:検索結果のリポジトリ
+    func presentSearchResult(results:[RepoResult])
+    
+    /// エラーを表示する
+    /// - parameter error:検索の際に発生したエラー
+    func presentSearchError(error:SearchError)
 }
 
 
