95a34d0d6ebd478d8866c743191d27b7d81b9e75
kyoya <kyoyaprog@gmail.com>
2021-02-22 17:02:30 +0900

    RepoUsecaseProtocolの作成

---
+++ Clean Architecture/RepoUsecase.swift
@@ -0,0 +1,15 @@
+//
+//  RepoUsecase.swift
+//  Clean Architecture
+//
+//  Created by 松山響也 on 2021/02/22.
+//
+
+import Foundation
+
+/// リポジトリ検索　Usecase Input
+protocol RepoUsecaseProtocol{
+    func startSearch(keyword:String)
+}
+
+
